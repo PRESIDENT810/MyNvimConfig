f3f4e8d1da4615db1b9a21505151c561b5b0f537
Kaining Zhong <kainzhong@tesla.com>
2024-07-25 14:20:45 -0700

    Update

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+        "clang-format",
+        "clangd",
+        "cmake-language-server",
+        "cmakelang",
+        "cmakelint",
+        "cpplint",
+        "cpptools",
+        "eslint_d",
+        "flake8",
+        "jq",
+        "lua-language-server",
+        "prettier",
+        "swiftlint",
+        "typescript-language-server",
+      },
+    },
+  },
+}
