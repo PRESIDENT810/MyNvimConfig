1947712c200f837cf6c6b612b79f5f78ae1a3f3e
Kaining Zhong <kainzhong@tesla.com>
2024-07-30 17:43:55 -0700

    Update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,3 +8,16 @@ vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
     vim.bo.filetype = "ruby"
   end
 })
+
+-- 开启 Folding
+vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+
+local wk = require("which-key")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
+
+wk.add({
+  {"<leader>fs", desc = "Document Symbols"}
+})
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,12 +1,13 @@
 return {
-  -- add GitHub Copilot
-  {
-    "github/copilot.vim",
-    cmd = "Copilot",
-    keys = { { "<leader>cp", "<cmd>Copilot<cr>", desc = "GitHub Copilot" } },
-    opts = {
-      -- add your options that should be passed to the setup() function here
-      position = "right",
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  opts = {
+    suggestion = { enabled = false },
+    panel = { enabled = false },
+    filetypes = {
+      markdown = true,
+      help = true,
     },
   },
 }
