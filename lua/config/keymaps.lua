ee48c7839c03c3d272f4e09b871b6dd2742dbf46
Kaining Zhong <kainzhong@tesla.com>
2024-07-31 11:14:26 -0700

    Update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,9 +4,37 @@
 
 local wk = require("which-key")
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
+local telescope = require('telescope.builtin')
+
 
 wk.add({
-  {"<leader>fs", desc = "Document Symbols"}
+  { "<leader>fs", telescope.lsp_document_symbols, desc = "Document Symbols" }
+})
+
+
+wk.add({
+  { "<leader>t", desc = "telescope" },
+
+  {
+    mode = { "n", "v" },
+    { "<leader>tm", telescope.keymaps, desc = "keymaps" },
+    { "<leader>tq", telescope.quickfix, desc = "quickfix" },
+    { "<leader>tl", telescope.loclist, desc = "loclist" },
+    { "<leader>tf", telescope.find_files, desc = "find files" },
+    { "<leader>tg", telescope.live_grep, desc = "live grep" },
+    { "<leader>tb", telescope.buffers, desc = "buffers" },
+    { "<leader>th", telescope.help_tags, desc = "help tags" },
+    { "<leader>ts", telescope.lsp_document_symbols, desc = "document symbols" },
+    { "<leader>td", telescope.lsp_definitions, desc = "definitions" },
+    { "<leader>ti", telescope.lsp_implementations, desc = "implementations" },
+    { "<leader>tt", telescope.lsp_type_definitions, desc = "type definitions" },
+    { "<leader>tr", telescope.lsp_references, desc = "references" },
+    { "<leader>tc", telescope.commands, desc = "commands" },
+    { "<leader>tw", telescope.lsp_workspace_symbols, desc = "workspace symbols" },
+    { "<leader>to", telescope.lsp_document_diagnostics, desc = "document diagnostics" },
+    { "<leader>ta", telescope.lsp_workspace_diagnostics, desc = "workspace diagnostics" },
+    { "<leader>ty", telescope.lsp_dynamic_workspace_symbols, desc = "dynamic workspace symbols" },
+    { "<leader>te", telescope.lsp_workspace_code_actions, desc = "workspace code actions" },
+  }
+
 })
