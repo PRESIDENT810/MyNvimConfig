3fa6e4e225f7b7024504405667891175d4c6519e
Kaining Zhong <kainzhong@tesla.com>
2024-12-16 14:43:36 -0800

    Update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -43,48 +43,6 @@ require("lazy").setup({
   },
 })
 
-require("nvim-treesitter.configs").setup({
-  -- 安装 language parser
-  -- :TSInstallInfo 命令查看支持的语言
-  ensure_installed = {
-    "html",
-    "css",
-    "lua",
-    "javascript",
-    "typescript",
-    "tsx",
-    "cpp",
-    "java",
-    "go",
-    "rust",
-    "python",
-    "c",
-    "swift",
-    "json",
-    "ruby"
-  },
-  -- 启用代码高亮功能
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false
-  },
-  -- 启用增量选择
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = '<CR>',
-      node_incremental = '<CR>',
-      node_decremental = '<BS>',
-      scope_incremental = '<TAB>',
-    }
-  },
-  -- 启用基于Treesitter的代码格式化(=) . NOTE: This is an experimental feature.
-  indent = {
-    enable = true
-  }
-})
-
-
 -- 恢复最后一次设置的colorscheme
 local colorscheme_config = vim.fn.stdpath('config') .. '/lua/config/colorscheme.lua'
 if vim.fn.filereadable(colorscheme_config) == 1 then
@@ -93,7 +51,6 @@ end
 
 
 local telescope = require('telescope')
-local telescopeConfig = require("telescope.config")
 
 local search_hidden = false
 -- Clone the default Telescope configuration
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -48,4 +48,48 @@ return {
     },
   },
 
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "rust",
+        "go",
+        "cpp",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+      -- 启用代码高亮功能
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false
+      },
+      -- 启用增量选择
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<CR>',
+          node_incremental = '<CR>',
+          node_decremental = '<BS>',
+          scope_incremental = '<TAB>',
+        }
+      },
+      -- 启用基于Treesitter的代码格式化(=) . NOTE: This is an experimental feature.
+      indent = {
+        enable = true
+      }
+    },
+  },
+
 }
