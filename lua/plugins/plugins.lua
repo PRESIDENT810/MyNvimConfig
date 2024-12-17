5d7451aeb7e3c8f92c5a2fc9ecc4b69e719b5d37
Kaining Zhong <kainzhong@tesla.com>
2024-12-17 13:58:47 -0800

    Update

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -14,6 +14,13 @@ return {
   },
 
   {
+    "snacks.nvim",
+    opts = {
+      scroll = { enabled = false },
+    },
+  },
+
+  {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
